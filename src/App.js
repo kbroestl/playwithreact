6e77340f2f0efe478da0b80d5238e9313d2540d2
Kent Broestl <kbroestl@oh.hra.com>
2021-07-09 21:34:40 -0400

    Just starting to work with Axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
+console.log("hello world");
+
+const request = axios.get('https://api.openweathermap.org/data/2.5/weather?zip=43210,us&appid=224b7a38e454801d04793fddcdfa70e7')
+.then(response => {
+console.log(response.data.main.temp);
+});
+
 function App() {
   return (
     <div className="App">
