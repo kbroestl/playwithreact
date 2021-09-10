df104c75fa5cf230f6ddfef17d1cbe202f8e1528
Kent Broestl <kbroestl@oh.hra.com>
2021-09-10 15:50:26 -0400

    Revisions to the App for more locations.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import './App.css';
 
 import axios from 'axios';
 
-const key = process.env.REACT_APP_WEATHER_API_KEY;
+const api_key = process.env.REACT_APP_WEATHER_API_KEY;
+const api_url = `https://api.openweathermap.org/data/2.5/weather?appid=${api_key}&zip=`;
 
-axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=43210,us&appid=${key}`)
-.then(response => {
-console.log(response.data.main.temp);
-});
+const locations = [
+  "26135,de", // Oldenburg, DE
+  "43210,us", // Columbus, OH
+  "80513,us", // Berthoud, CO 
+  "49931,us" // Houghton, MI
+];
+
+
+/*
+pseudocode:
+* set up the locations we're going to care about
+* for each location, and output a div-ish container for each
+* fetch the current weather conditions for each location
+*/
+
+function fetch_location(zip) {
+  axios.get(`${api_url}${zip}`).then(response =>
+    console.log(response.data));
+}
+
+locations.forEach(place => {
+  fetch_location(place);
+})
 
 function App() {
   return (
